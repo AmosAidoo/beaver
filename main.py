2469fd5b576e97aacfd3eb4060942103281e8061
Amos Aidoo <aidooamos20@gmail.com>
2021-06-11 07:47:13 +0000

    send_response displays a directory listing if a directory is requested. Also a specific file is served on request

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ args = argumentParser.parse_args()
 
 HOST = "127.0.0.1" if args.address is None else args.address
 PORT = 8000 if args.port is None else args.port
-ROOT_DIR = os.getcwd() if args.root is None else args.root
+ROOT_DIR = "./" if args.root is None else args.root
 
 def parse_request(request):
     tokens = request.decode("utf8").split("\r\n")
@@ -42,45 +42,57 @@ def send_response(client_conn, request_line, headers, body):
     path = os.path.join(ROOT_DIR, request_line.split()[1][1:])
     
     if os.path.isdir(path):
-        print(os.listdir(path))
+        listing = os.listdir(path)
+        body += b"<h1>Directory listing</h1>" 
+        body += b"<ul>"
+        for item in listing:
+            body += b"<li>" + f"<a href=\"{path}{item}\">".encode("utf8") + item.encode("utf8") + b"</a></li>"
+        body += b"</ul>"
     elif os.path.isfile(path):
-        print("File exists")
+        file = open(path, "rb")
+        content = file.read()
+        body = content
+        file.close()
     else:
-        print("404 not found")
+        status_line = b"HTTP/1.1 404 NOT FOUND\n"
+        body = b"<p>Resourse could not be found on the server</p>"
 
+    # Form the http response
     response = status_line + headers + body
-    client_conn.send(response)
-
+    
+    print(response)
+    
+    client_conn.sendall(response)
 
 print(f"Listening at port {PORT}")
 
-# while True:
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
 
-    conn, addr = s.accept()
-    
-    with conn:
-        print("Connected by", addr)
-        request = b""
-        BUFFER = 1024
+        conn, addr = s.accept()
+        
+        with conn:
+            print("Connected by", addr)
+            request = b""
+            BUFFER = 1024
 
-        # Read the request from the client
-        while True:
-            data = conn.recv(BUFFER)
-            length = len(data)
-            request += data
-            
-            if data == b"":
-                break
-            
-            # This is to detect the end of the request
-            # It seems to work though there might be some corner cases
-            # I will write some tests
-            if length < BUFFER and data[length - 1] == 0xA:
-                break
+            # Read the request from the client
+            while True:
+                data = conn.recv(BUFFER)
+                length = len(data)
+                request += data
+                
+                if data == b"":
+                    break
+                
+                # This is to detect the end of the request
+                # It seems to work though there might be some corner cases
+                # I will write some tests
+                if length < BUFFER and data[length - 1] == 0xA:
+                    break
 
-        request_line, headers, body = parse_request(request)
+            request_line, headers, body = parse_request(request)
 
-        send_response(conn, request_line, headers, body)
+            send_response(conn, request_line, headers, body)
